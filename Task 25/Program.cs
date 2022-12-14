df3544640d3b01ca025c9abe25064f721fe2bb02
Elena <el.kalenova@mail.ru>
2022-12-14 22:36:12 +0300

    Четвертое домашнее задание

---
+++ Task 25/Program.cs
@@ -0,0 +1,10 @@
+﻿// task 25
+Console.Clear();
+Console.Write("Введите число А: ");
+double A = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+double C = 1;
+for (int i = 1; i <= B; i++)
+    C = C * A;
+Console.WriteLine("А в степени B равно: " + C);
---
+++ Task 29/Program.cs
@@ -0,0 +1,9 @@
+﻿// task 29
+Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+double [] array = new double[n];
+Console.WriteLine("Введите элементы массива: ");
+for (int i = 0; i < array.Length; i++)
+    array[i] = Convert.ToDouble(Console.ReadLine()); 
+Console.WriteLine($"[{string.Join("; ", array)}]");
